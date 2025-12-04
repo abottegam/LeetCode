7657a906c518e374d81ff39ff42dbcd75e5b24f0
Adriana Bottega <abottega@outlook.com>
2025-12-04 01:26:31 -0500

    Added another problem

---
+++ Prob242/Valid_Anagram.cpp
@@ -0,0 +1,35 @@
+/*
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+ 
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+
+Output: true
+
+Example 2:
+
+Input: s = "rat", t = "car"
+
+Output: false
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        
+    }
+};
+
+int main(){
+    Solution solution;
+    string s = "anagram";
+    string t = "nagaram";
+    solution.isAnagram(s,t);
+}
