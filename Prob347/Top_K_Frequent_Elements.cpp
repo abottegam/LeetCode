64f9c8dcef452d11926753446c3e1ba4f9a5f960
Adriana Bottega <65979221+abottegam@users.noreply.github.com>
2025-12-05 16:19:44 -0500

    Prob 347

--- Prob347/Top_K_Frequent_Elements.cpp
+++ Prob347/Top_K_Frequent_Elements.cpp
@@ -42,6 +42,7 @@ Follow up: Your algorithm's time complexity must be better than O(n log n), wher
 #include <unordered_map>
 #include <utility>
 #include <queue>
+#include <bits/stdc++.h>
 
 using namespace std;
 
@@ -86,7 +87,49 @@ public:
         // return top;
 
         //---------------------------Second attempt---------------------------
-        unordered_map<int, int> map;
+
+        //     vector<int> map1;
+        //     //finding max element
+        //     int max = 0;
+        //     for (int num : nums){
+        //         if(abs(num) > max){
+        //             max = abs(num);
+        //         }
+        //     }
+        //     //assigning size of map to max element and initializing to 0
+        //     map1.assign(max, 0);
+
+        //     //counting frequencies
+        //     for (int num : nums)
+        //     {
+        //         map1[abs(num)-1]++;
+        //     }
+
+        //     //organizing
+        //     priority_queue<pair<int, int>> p1;
+        //     for (int i = 0; i<map1.size(); i++)
+        //     {
+        //         p1.push(make_pair(map1.at(i), i+1));
+        //     }
+
+        //     map1.erase(map1.begin(), map1.end());
+
+        //     for (size_t i = 0; i < k; i++)
+        //     {
+        //         map1.push_back(p1.top().second);
+        //         p1.pop();
+        //     }
+        //     return map1;
+
+        // }
+
+        //---------------------------Third attempt---------------------------
+        /*
+        Runtime
+        0 ms | Beats 100.00%
+        O(NlogN)
+        */
+        unordered_map<int,int> map;
         for(int num: nums){
             if (!map.count(num))
             {
@@ -96,23 +139,48 @@ public:
                 map[num]++;
             }
         }
+
+        priority_queue<pair<int,int>, vector<pair<int,int>>, myComp> p1;
+
+        for (auto pair:map)
+        {
+            p1.push(pair);
+        }
+        vector<int> ans;
         for (size_t i = 0; i < k; i++)
         {
-            
+            ans.push_back(p1.top().first);
+            p1.pop();
         }
         
+        return ans;
     }
+    struct myComp
+    {
+        constexpr bool operator()(
+            pair<int, int> const &a,
+            pair<int, int> const &b)
+            const noexcept
+        {
+            if(a.second == b.second){
+                return b.second;
+            }
+            return a.second < b.second;
+        }
+    };
 };
 
 int main(){
     Solution solution;
-    vector<int> nums = { 1, 1, 1, 2, 2, 3 };
-    int k = 2;
+    vector<int> nums = {-1,-1};
+    int k = 1;
     vector<int> reply = solution.topKFrequent(nums, k);
-    cout << "[ ";
+    string print = "[ ";
     for (auto i : reply)
     {
-        cout << i << ", ";
+        print = print + to_string(i) + ", ";
     }
-    cout << "]";
+    print.erase(print.end()-2, print.end()-1);
+    print+= "]";
+    cout << print;
 }
