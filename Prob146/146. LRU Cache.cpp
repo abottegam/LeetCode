f48e70cd53065c03a0a7db6686605b998eb7fad9
Adriana Bottega <65979221+abottegam@users.noreply.github.com>
2025-12-11 16:50:20 -0500

    more updates

--- Prob146/146. LRU Cache.cpp
+++ Prob146/146. LRU Cache.cpp
@@ -77,6 +77,20 @@ public:
     void put(int key, int value)
     {
         Node *temp = new Node;
+        if (cache.size() + 1 > size)
+        {
+            if (cache.find(key) != cache.end())
+            {
+                //if a key is already found in cache, change the value and push to front
+                temp = cache.at(key);
+                temp->data.second = value;
+                pushToFront(temp);
+                return;
+            }
+            else{
+                deleteFromBack();
+            }
+        }
         cache[key] = temp; //if key diff than value add the difference in
         temp->data = make_pair(key,value);
         temp->next = nullptr;
@@ -88,10 +102,6 @@ public:
         }
         else
         {
-            if (cache.size() > size)
-            {
-                deleteFromBack();
-            }
             if(size == 1){
                 head = tail = temp;
             }
@@ -101,7 +111,16 @@ public:
                 head = temp;
             }
         }
-    }
+        //print linked list
+        Node* first = head;
+        cout << "Key = " << key << " Value = " << value << endl;
+        while (first != tail)
+        {
+            cout << "( " << first->data.first << ", " << first->data.second << " ),";
+            first = first->next;
+        }
+        cout << "( " << first->data.first << ", " << first->data.second << " )" << endl;
+        }
 //to push to front after get
     void pushToFront(Node *nuevo)
     {
@@ -143,14 +162,32 @@ public:
 
 int main()
 {
-    LRUCache *obj = new LRUCache(2);
-    obj->put(1, 1);
-    obj->put(2, 2);
-    int param_1 = obj->get(1); // return 1
-    obj->put(3, 3);            // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
-    int param_2 = obj->get(2); // returns -1 (not found)
-    obj->put(4, 4);            // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
-    int param_3 = obj->get(1); // return -1 (not found)
-    int param_4 = obj->get(3); // return 3
-    int param_5 = obj->get(4); // return 4
+    // LRUCache *obj = new LRUCache(2);
+    // obj->put(1, 1);
+    // obj->put(2, 2);
+    // int param_1 = obj->get(1); // return 1
+    // obj->put(3, 3);            // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
+    // int param_2 = obj->get(2); // returns -1 (not found)
+    // obj->put(4, 4);            // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
+    // int param_3 = obj->get(1); // return -1 (not found)
+    // int param_4 = obj->get(3); // return 3
+    // int param_5 = obj->get(4); // return 4
+
+    LRUCache *obj = new LRUCache(10);
+    obj->put(10, 13);
+    obj->put(3, 17);
+    obj->put(6, 11);            // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
+    obj->put(10, 5);            // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
+    obj->put(9, 10);
+    int param_1 = obj->get(13); // return -1
+    obj->put(2, 19);
+    int param_4 = obj->get(2); // return 3
+    int param_5 = obj->get(3);
+    obj->put(5, 25);
+    int param_5 = obj->get(8);
 }
+
+/*
+[[10],[10,13],[3,17],[6,11],[10,5],[9,10],[13],[2,19],[2],[3],[5,25],
+[8],[9,22],[5,5],[1,30],[11],[9,12],[7],[5],[8],[9],[4,30],[9,3],[9],[10],[10],[6,14],[3,1],[3],[10,11],[8],[2,14],[1],[5],[4],[11,4],[12,24],[5,18],[13],[7,23],[8],[12],[3,27],[2,12],[5],[2,9],[13,4],[8,18],[1,7],[6],[9,29],[8,21],[5],[6,30],[1,12],[10],[4,15],[7,22],[11,26],[8,17],[9,29],[5],[3,4],[11,30],[12],[4,29],[3],[9],[6],[3,4],[1],[10],[3,29],[10,28],[1,20],[11,13],[3],[3,12],[3,8],[10,9],[3,26],[8],[7],[5],[13,17],[2,27],[11,15],[12],[9,19],[2,15],[3,16],[1],[12,17],[9,1],[6,19],[4],[5],[5],[8,1],[11,7],[5,2],[9,28],[1],[2,2],[7,4],[4,22],[7,24],[9,26],[13,28],[11,26]]
+*/
