60b2f8419c7d60e72d0d7da0f7643a58d75157f3
Adriana Bottega <abottega@outlook.com>
2025-12-11 23:34:28 -0500

    Prob. 146: LRU Cache; Runtime: 88%, Memory: 88%

--- Prob146/146. LRU Cache.cpp
+++ Prob146/146. LRU Cache.cpp
@@ -42,12 +42,17 @@ At most 2 * 105 calls will be made to get and put.
 #include <utility>
 
 using namespace std;
+/*
+Runtime: 57 ms, faster than 88.14%
+Memory Usage: 172.95 MB, less than 88.14%
+*/
 
 struct Node
 {
-    pair<int,int> data;
+    pair<int, int> data;
     Node *next;
     Node *prev;
+    Node(pair<int, int> content) : data(content), prev(nullptr), next(nullptr) {}
 };
 
 class LRUCache
@@ -65,91 +70,99 @@ public:
 
     int get(int key)
     {
-        unordered_map<int, Node *>::const_iterator got = cache.find(key);
+        auto got = cache.find(key);
         if (got == cache.end())
         {
+            cout << "Got null from " << key << endl;
             return -1;
         }
-        pushToFront(got->second);
+        if (got->second != head)
+            pushToFront(got->second);
+        cout << "Got " << got->second->data.second << " from " << key << endl;
         return got->second->data.second;
     }
 
     void put(int key, int value)
     {
-        Node *temp = new Node;
+        if (cache.find(key) != cache.end())
+        {
+            Node *node = cache.at(key);
+            node->data.second = value;
+            if (node != head)
+                pushToFront(node);
+            return;
+        }
+
         if (cache.size() + 1 > size)
         {
-            if (cache.find(key) != cache.end())
-            {
-                //if a key is already found in cache, change the value and push to front
-                temp = cache.at(key);
-                temp->data.second = value;
-                pushToFront(temp);
-                return;
-            }
-            else{
-                deleteFromBack();
-            }
+            cache.erase(tail->data.first);
+            deleteFromBack();
         }
-        cache[key] = temp; //if key diff than value add the difference in
-        temp->data = make_pair(key,value);
-        temp->next = nullptr;
-        temp->prev = nullptr;
+        Node *temp = new Node(make_pair(key, value));
 
-        if (head == nullptr)
+        if (!head)
         {
             head = tail = temp;
+            // cout << "Key = " << key << " Value = " << value << endl;
+            // cout << "( " << head->data.first << ", " << head->data.second << " )"
+            //      << endl;
         }
         else
         {
-            if(size == 1){
-                head = tail = temp;
-            }
-            else{
-                head->prev = temp;
-                temp->next = head;
-                head = temp;
-            }
+            head->prev = temp;
+            temp->next = head;
+            head = temp;
         }
-        //print linked list
-        Node* first = head;
+        cache[key] = temp;
+
+        // for debugging purposes
+        Node *first = head;
         cout << "Key = " << key << " Value = " << value << endl;
         while (first != tail)
         {
-            cout << "( " << first->data.first << ", " << first->data.second << " ),";
+            cout << "( " << first->data.first << ", " << first->data.second
+                 << " ),";
             first = first->next;
         }
-        cout << "( " << first->data.first << ", " << first->data.second << " )" << endl;
-        }
-//to push to front after get
-    void pushToFront(Node *nuevo)
+        cout << "( " << first->data.first << ", " << first->data.second << " )"
+             << endl;
+    }
+    // to push recently accessed to front after get
+    void pushToFront(Node *recAcc)
     {
-        if (nuevo != head)
-        {
-            nuevo->prev->next = nuevo->next;
-            if (nuevo == tail)
-                tail = nuevo->prev;
-            else
-                nuevo->next->prev = nuevo->prev;
-            head->prev = nuevo;
-            nuevo->next = head;
-            nuevo->prev = nullptr;
-            head = nuevo;
+        if (recAcc == tail)
+        { // if node is tail
+            tail = recAcc->prev;
+            tail->next = nullptr;
         }
-        
+        else
+        {
+            recAcc->prev->next = recAcc->next;
+            recAcc->next->prev = recAcc->prev;
+        }
+
+        recAcc->next = head;
+        recAcc->prev = nullptr;
+
+        head->prev = recAcc;
+        head = recAcc;
     }
 
     void deleteFromBack()
     {
-        cache.erase(tail->data.first); // erase the key from map
-        if(tail == head){
-            tail = nullptr;
-            head = nullptr;
+        Node* old = tail;
+        if (tail == head)
+        {
+            tail = head = nullptr;
         }
-        else{
-            tail->prev->next = nullptr;
+        else
+        {
             tail = tail->prev;
+            tail->next = nullptr;
         }
+        old->prev = nullptr;
+        old->next = nullptr;
+        delete old;
     }
 };
 
@@ -162,32 +175,45 @@ public:
 
 int main()
 {
-    // LRUCache *obj = new LRUCache(2);
-    // obj->put(1, 1);
-    // obj->put(2, 2);
-    // int param_1 = obj->get(1); // return 1
-    // obj->put(3, 3);            // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
-    // int param_2 = obj->get(2); // returns -1 (not found)
-    // obj->put(4, 4);            // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
-    // int param_3 = obj->get(1); // return -1 (not found)
-    // int param_4 = obj->get(3); // return 3
-    // int param_5 = obj->get(4); // return 4
-
-    LRUCache *obj = new LRUCache(10);
-    obj->put(10, 13);
-    obj->put(3, 17);
-    obj->put(6, 11);            // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
-    obj->put(10, 5);            // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
-    obj->put(9, 10);
-    int param_1 = obj->get(13); // return -1
-    obj->put(2, 19);
-    int param_4 = obj->get(2); // return 3
-    int param_5 = obj->get(3);
-    obj->put(5, 25);
-    int param_5 = obj->get(8);
+    LRUCache *obj = new LRUCache(2);
+    int param_1 = obj->get(2);
+    obj->put(2, 6);
+    int param_2 = obj->get(1);
+    obj->put(1, 5);
+    obj->put(1, 2);            // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
+    int param_3 = obj->get(1); // returns -1 (not found)
+    int param_4 = obj->get(2); // return -1 (not found)
+    // LRUCache *obj = new LRUCache(10);
+    // obj->put(10, 13);
+    // obj->put(3, 17);
+    // obj->put(6, 11);            // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
+    // obj->put(10, 5);            // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
+    // obj->put(9, 10);
+    // int param_1 = obj->get(13); // return -1
+    // obj->put(2, 19);
+    // int param_4 = obj->get(2); // return 3
+    // int param_5 = obj->get(3);
+    // obj->put(5, 25);
+    // int param_3 = obj->get(8);
+    // obj->put(9, 22);
+    // obj->put(5, 5);
+    // obj->put(1, 30);
+    // int param_6 = obj->get(11);
+    // obj->put(9, 12);
+    // int param_7 = obj->get(7);
+    // int param_8 = obj->get(5);
+    // int param_9 = obj->get(8);
+    // int param_10 = obj->get(9);
+    // obj->put(4, 30);
+    // obj->put(9, 3);
+    // int param_11 = obj->get(9);
+    // int param_12 = obj->get(10);
+    // int param_13 = obj->get(10);
+    // obj->put(9, 12);
+    // int param_14 = obj->get(10);
 }
 
 /*
-[[10],[10,13],[3,17],[6,11],[10,5],[9,10],[13],[2,19],[2],[3],[5,25],
-[8],[9,22],[5,5],[1,30],[11],[9,12],[7],[5],[8],[9],[4,30],[9,3],[9],[10],[10],[6,14],[3,1],[3],[10,11],[8],[2,14],[1],[5],[4],[11,4],[12,24],[5,18],[13],[7,23],[8],[12],[3,27],[2,12],[5],[2,9],[13,4],[8,18],[1,7],[6],[9,29],[8,21],[5],[6,30],[1,12],[10],[4,15],[7,22],[11,26],[8,17],[9,29],[5],[3,4],[11,30],[12],[4,29],[3],[9],[6],[3,4],[1],[10],[3,29],[10,28],[1,20],[11,13],[3],[3,12],[3,8],[10,9],[3,26],[8],[7],[5],[13,17],[2,27],[11,15],[12],[9,19],[2,15],[3,16],[1],[12,17],[9,1],[6,19],[4],[5],[5],[8,1],[11,7],[5,2],[9,28],[1],[2,2],[7,4],[4,22],[7,24],[9,26],[13,28],[11,26]]
+[[10],[10,13],[3,17],[6,11],[10,5],[9,10],[13],[2,19],[2],[3],[5,25],[8],[9,22],[5,5],[1,30],[11],[9,12],[7],[5],[8],[9],[4,30],[9,3],[9],[10],[10],
+[6,14],[3,1],[3],[10,11],[8],[2,14],[1],[5],[4],[11,4],[12,24],[5,18],[13],[7,23],[8],[12],[3,27],[2,12],[5],[2,9],[13,4],[8,18],[1,7],[6],[9,29],[8,21],[5],[6,30],[1,12],[10],[4,15],[7,22],[11,26],[8,17],[9,29],[5],[3,4],[11,30],[12],[4,29],[3],[9],[6],[3,4],[1],[10],[3,29],[10,28],[1,20],[11,13],[3],[3,12],[3,8],[10,9],[3,26],[8],[7],[5],[13,17],[2,27],[11,15],[12],[9,19],[2,15],[3,16],[1],[12,17],[9,1],[6,19],[4],[5],[5],[8,1],[11,7],[5,2],[9,28],[1],[2,2],[7,4],[4,22],[7,24],[9,26],[13,28],[11,26]]
 */
